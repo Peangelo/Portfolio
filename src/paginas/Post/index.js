611cc583398f139f62cd64fa41d65755ef8eead9
Peangelo <peangelorafa@gmail.com>
2024-08-27 11:10:31 -0300

    criando posts

---
+++ src/paginas/Post/index.js
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom"
+import posts from "json/posts.json"
+
+export default function Post() {
+    const parametros = useParams()
+
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id)
+    })
+    console.log(post)
+    return (
+        <h1>{parametros.id}</h1>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
 
 
 
@@ -17,6 +18,7 @@ function AppRoutes() {
           <Route path="/" element={<PaginaPadrao/>}>
             <Route path="/" element={<Inicio/>}/>
             <Route path="/sobremim" element={<SobreMim/>}/>
+            <Route path="posts/:id" element={<Post/>}/>
           </Route>
           <Route path="*" element={<div>Pagina não encontrada</div>}/>
         </Routes>
