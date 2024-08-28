949450a0ab22c6d2d817e4e53ae024e6a579bde9
Peangelo <peangelorafa@gmail.com>
2024-08-28 09:58:13 -0300

    criacao da pagina 404

---
+++ src/paginas/NaoEncontrada/index.js
@@ -0,0 +1,33 @@
+import styles from "./NaoEncontrada.module.css"
+import erro404 from 'assets/erro_404.png'
+
+export default function NaoEncontrada() {
+
+    return (
+
+        <>
+        
+        
+        <div className={styles.conteudoContainer}>
+            <span className={styles.texto404}>404</span>
+            <h1 className={styles.titulo}>
+                Ops! Pagina não encontrada
+            </h1>
+            <p className={styles.paragrafo}>
+                Tem certeza que é isso que esta procurando?
+            </p>
+
+            <div className={styles.botaoContainer}>
+                <button>Voltar</button>
+            </div>
+            <img
+                className={styles.imagemCachorro}
+                src={erro404}
+                alt="cachorro de oculos"
+            />
+          
+        </div>
+        <div className={styles.espacoEmBranco}></div>
+        </>
+    )
+}
